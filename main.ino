e5d957018afedb83b7a140c5e23f844e9e6c20a0
Ruben <cyclops-github@prof-x.net>
2022-03-18 09:25:29 +0000

    First working example.

--- main.ino
+++ main.ino
@@ -1,26 +1,46 @@
 
-#include "Arduino.h"
+#include <Arduino.h>
+#include <VL53L0X.h>
+#include <Wire.h>
 #include "rubendisplay.h"
 
-int count;
 RubenDisplay *disp;
+VL53L0X sensor;
 
 void setup()
 {
 	Serial.begin(115200);
 	Serial.printf("Booting version: 0.1\n");
+	Wire.begin();
+
+	pinMode(Vext, OUTPUT);
+	digitalWrite(Vext, LOW); // set vext to high
+
 	disp = new RubenDisplay();
-	count = 0;
+
+	sensor.setTimeout(500);
+	while (!sensor.init())
+	{
+
+		Serial.println("Failed to detect and initialize sensor!");
+		delay(1000);
+	}
 }
 
 void loop()
 {
-	Serial.printf("Count: %d\n", count);
+	byte error, address;
+	int nDevices;
 
-	count++;
-	char text[11];
-	snprintf(text, 11, "TEST %d", count);
+	Serial.println("Scanning...");
+	if (sensor.timeoutOccurred())
+	{
+		Serial.print(" TIMEOUT");
+	}
+
+	char text[30];
+	snprintf(text, 30, "Distance: %dmm", sensor.readRangeSingleMillimeters());
 	Serial.println(text);
 	disp->WriteOut(text);
-	delay(100);
+	delay(1000);
 }
